fc58bd1d8c1bd23a925b6f02ed8b04f2835abf4f
Robert Wearden <robertwearden2@gmail.com>
2021-06-10 16:20:59 +0100

    add ability to query db by city

--- App.js
+++ App.js
@@ -7,13 +7,13 @@ import { View, Text } from "react-native";
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCypVoinAdRkCENQfn2AqR6ebT-11sovAs",
-  authDomain: "cleaner-3207e.firebaseapp.com",
-  projectId: "cleaner-3207e",
-  storageBucket: "cleaner-3207e.appspot.com",
-  messagingSenderId: "98775205470",
-  appId: "1:98775205470:web:0d767f25351083f90556cf",
-  measurementId: "G-MC62M5YJD9",
+	apiKey: "AIzaSyCypVoinAdRkCENQfn2AqR6ebT-11sovAs",
+	authDomain: "cleaner-3207e.firebaseapp.com",
+	projectId: "cleaner-3207e",
+	storageBucket: "cleaner-3207e.appspot.com",
+	messagingSenderId: "98775205470",
+	appId: "1:98775205470:web:0d767f25351083f90556cf",
+	measurementId: "G-MC62M5YJD9"
 };
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -29,30 +29,30 @@ const db = firebase.initializeApp(firebaseConfig).firestore(); // <--- changed f
 const Stack = createStackNavigator();
 
 const App = () => {
-  var citiesRef = db.collection("cities");
+	var citiesRef = db.collection("cities");
 
-  citiesRef.doc("SF").set({
-    name: "San Francisco",
-    state: "CA",
-    country: "USA",
-    capital: false,
-    population: 860000,
-    regions: ["west_coast", "norcal"],
-  });
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Landing">
-        <Stack.Screen
-          name="Landing"
-          component={LandingScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	citiesRef.doc("SF").set({
+		name: "San Francisco",
+		state: "CA",
+		country: "USA",
+		capital: false,
+		population: 860000,
+		regions: ["west_coast", "norcal"]
+	});
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Landing">
+				<Stack.Screen
+					name="Landing"
+					component={LandingScreen}
+					options={{ headerShown: false }}
+				/>
+				<Stack.Screen name="Register" component={RegisterScreen} />
+				<Stack.Screen name="Login" component={LoginScreen} />
+				<Stack.Screen name="Home" component={HomeScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 };
 export default App;
 // start here
