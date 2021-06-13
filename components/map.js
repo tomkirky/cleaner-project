bbb613304f0fd783d7797987524d09527ec28901
Piotr-Szyszkowski <79057797+Piotr-Szyszkowski@users.noreply.github.com>
2021-06-13 13:16:12 +0100

    Update map.js

--- components/map.js
+++ components/map.js
@@ -6,52 +6,54 @@ import { googleMapsAPI } from "../config";
 import { useState, useEffect } from "react";
 
 const Map = () => {
-	const [coordinates, setCoordinates] = useState({
-		lat: "",
-		lng: ""
-	});
+  const [coordinates, setCoordinates] = useState({
+    lat: "",
+    lng: "",
+  });
 
-	useEffect(() => {
-		axios
-			.get(
-				`https://maps.googleapis.com/maps/api/geocode/json?address=24%20Sussex%20Drive%20Ottawa%20ON&key=${googleMapsAPI}`
-			)
-			.then((result) => {
-				const { lat, lng } = result.data.results[0].geometry.location;
-				setCoordinates((currCoordinates) => {
-					return { ...currCoordinates, lat: lat, lng: lng };
-				});
-			});
-	}, [setCoordinates]);
-	// ^^^ getting the response but struggling to set State
+  useEffect(() => {
+    axios
+      .get(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=24%20Sussex%20Drive%20Ottawa%20ON&key=${googleMapsAPI}`
+      )
+      .then((result) => {
+        const { lat, lng } = result.data.results[0].geometry.location;
+        setCoordinates((currCoordinates) => {
+          return { ...currCoordinates, lat: lat, lng: lng };
+        });
+      });
+  }, [setCoordinates]);
+  // ^^^ getting the response but struggling to set State
 
-	console.log(coordinates.lat);
-	https: return (
-		<View style={styles.container}>
-			<MapView
-				style={styles.map}
-				initialRegion={{
-					latitude: coordinates.lat,
-					longitude: coordinates.lng,
-					latitudeDelta: 0.07,
-					longitudeDelta: 0.07
-				}}
-			/>
-		</View>
-	);
+  console.log(coordinates.lat);
+  https: return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: coordinates.lat,
+          longitude: coordinates.lng,
+          latitudeDelta: 0.07,
+          longitudeDelta: 0.07,
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center"
-	},
-	map: {
-		width: Dimensions.get("window").width,
-		height: Dimensions.get("window").height
-	}
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
 });
 
 export default Map;
+
+// testing gitignore
