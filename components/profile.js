088c31e7d7e6867522fb5785e6d68b990239203d
tomkirky <tomk1989.tk@gmail.com>
2021-06-15 14:02:37 +0100

    Added some stying to profile page

--- components/profile.js
+++ components/profile.js
@@ -2,14 +2,35 @@ import { useState, useEffect } from "react";
 import React from "react";
 import { db, auth } from "../firebase";
 import firebase from "firebase";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Button, Title, Text } from "react-native-paper";
 
 const Profile = ({ cleaner, navigation }) => {
 	console.log(cleaner);
 
 	return (
 		<View>
-			<Text>Profile Page</Text>
+			<Title style={{ padding: 10, alignSelf: "center" }}>
+				{cleaner.companyName}
+			</Title>
+			<Text style={{ padding: 10, alignSelf: "center" }}>
+				{cleaner.companyDescription}
+			</Text>
+			<Button
+				icon="message-outline"
+				mode="contained"
+				style={{ padding: 10, justifyContent: "center", margin: 10 }}
+			>
+				Chat to {cleaner.companyName}
+			</Button>
+			<Button
+				icon="credit-card-outline"
+				mode="contained"
+				color="green"
+				style={{ padding: 10, justifyContent: "center", margin: 10 }}
+			>
+				Pay {cleaner.companyName}
+			</Button>
 		</View>
 	);
 };
